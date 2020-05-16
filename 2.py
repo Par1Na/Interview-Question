ef103f929a8ed2af67473dbecf3b25dec4d0a4fb
Par1Na <65071608+Par1Na@users.noreply.github.com>
2020-05-16 13:01:55 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,14 @@
 # Interview-Question
+
+
+def segregate(arr): 
+
+    res = ([x for x in arr if x==0] + [x for x in arr if x==1]) 
+
+    print(res) 
+
+if __name__ == "__main__": 
+
+    arr = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0]  
+
+    segregate(arr) 
