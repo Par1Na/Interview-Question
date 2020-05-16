a64391734df43a0def9f8d14bec64f28fa487ef3
Par1Na <65071608+Par1Na@users.noreply.github.com>
2020-05-16 13:16:02 +0000

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,1 +1,31 @@
 # Interview-Question
+
+
+
+def calculateSum(n) : 
+
+    if (n <= 0) : 
+
+        return 0
+
+    fibo =[0] * (n+1) 
+
+    fibo[1] = 1
+
+    sm = fibo[0] + fibo[1] 
+
+    for i in range(2,n+1) : 
+
+        fibo[i] = fibo[i-1] + fibo[i-2] 
+
+        sm = sm + fibo[i] 
+
+    return sm 
+
+n = 4
+
+print("Sum of Fibonacci numbers is : " , 
+
+      calculateSum(n)) 
+
+  
